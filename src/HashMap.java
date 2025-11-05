019081352ecd5c2b4d178320917f41066628f23f
L-Tran <logan.tran@menloschool.org>
2025-11-05 11:55:16 -0800

    Found table size and started add & get functions

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,8 +1,7 @@
 public class HashMap {
 
     public static final int EXTENDED_ASCII = 256;
-    public static final long P = 29245776433644439L;
-    public static final int DEFAULT_TABLE_SIZE = 2;
+    public static final int DEFAULT_TABLE_SIZE = 997;
 
     int tableSize;
     int n;
@@ -11,30 +10,59 @@ public class HashMap {
 
     public HashMap() {
         tableSize = DEFAULT_TABLE_SIZE;
+        keys = new String[DEFAULT_TABLE_SIZE];
+        values = new String[DEFAULT_TABLE_SIZE];
+        n = 0;
+
 
     }
 
-
-    public long hash(String s) {
+    public int hash(String s) {
         int length = s.length();
-        long h = 0;
+        int h = 0;
         // Preform Horner's Method
         for (int i = 0; i < length; i++) {
-            h = (h * EXTENDED_ASCII + s.charAt(i)) % P;
+            h = (h * EXTENDED_ASCII + s.charAt(i)) % tableSize;
         }
         return h;
     }
 
     void add(String key, String value) {
         // Linear Probing
-
+        n++;
+        int keyHash = hash(key);
+        while(keys[keyHash] != null) {
+            if(keyHash == tableSize) {
+                keyHash = 0;
+            }
+            else {
+                keyHash++;
+            }
+        }
+        keys[keyHash] = key;
+        values[keyHash] = value;
     }
+
     String get(String key) {
         // return value of key
+        int keyHash = hash(key);
+        while(!keys[keyHash].equals(key)) {
+            keyHash++;
+        }
+        return values[keyHash];
 
     }
     void resize() {
-        tableSize *= 2;
+        String[]tempKeys = keys;
+        String[]tempValues = values;
+        if (n == tableSize / 2) {
+            tableSize *= 2;
+            keys =  new String[tableSize];
+            values = new String[tableSize];
+        }
+        for(int i = 0 ; i < ) {
+
+        }
     }
 
 }
